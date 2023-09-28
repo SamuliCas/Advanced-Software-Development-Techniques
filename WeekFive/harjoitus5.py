0003d73e5d3c8c9058f057355fdd8f66920f3c84
Samulikoulu <t1casa00@students.oamk.fi>
2023-09-28 12:14:01 +0300

    jee

--- WeekFive/harjoitus5.py
+++ WeekFive/harjoitus5.py
@@ -1,28 +1,32 @@
 import tkinter as tk
 import winsound
 import time
-from PIL import Image,ImageTk
+from PIL import Image, ImageTk
 import threading
+import random
 
-window=tk.Tk()
+window = tk.Tk()
 window.title("Exercise 5")
 window.geometry("800x800")
 
-island = tk.Canvas(window, width=100, height=500, bg="green")
+island = tk.Canvas(window, width=100, height=500, bg="yellow")
 island.grid(row=1, column=1, rowspan=5)
 island.create_text(50, 250, text="island", fill="white", font=("Arial", 12, "bold"))
 
-continent = tk.Canvas(window, width=100, height=500, bg="blue")
+continent = tk.Canvas(window, width=100, height=500, bg="purple")
 continent.grid(row=1, column=5, rowspan=5)
 continent.create_text(50, 250, text="continent", fill="white", font=("Arial", 12, "bold"))
 
-monkey = tk.Canvas(window, width=100, height=100, bg="red")
-monkey.create_text(50, 50, text="monkey", fill="white", font=("Arial", 12, "bold"))
+erne_monkey = tk.Canvas(window, width=100, height=100, bg="brown")
+erne_monkey.create_text(50, 50, text="monkey", fill="white", font=("Arial", 12, "bold"))
+
+kerne_monkey = tk.Canvas(window, width=100, height=100, bg="red")
+kerne_monkey.create_text(50, 50, text="monkey", fill="white", font=("Arial", 12, "bold"))
 
 word_label = tk.Label(window, text="", font=("Arial", 12, "bold"))
 word_label.grid(row=9, column=5)
 
-emergencyMessage="We are stuck on the island please send help"
+emergencyMessage = "We are stuck on the island please send help"
 word_to_teach = ""
 
 def move_erne_monkey():
@@ -35,33 +39,32 @@ def move_erne_monkey():
     distance_x = x_target - x_start
     step_size = distance_x / 100  # Adjust the step size as needed
 
-    # Move the monkey horizontally
     for step in range(100):
         x = x_start + step * step_size
-        monkey.place(x=x, y=y_start)
-        winsound.Beep(440,100)
+        erne_monkey.place(x=x, y=y_start)
+        winsound.Beep(440, 100)
         window.update()  # Update the window to show the new position
         window.after(50)  # Delay for smoother animation (adjust as needed)
 
-        #Prints monkey movement
-        print(f"Step {step + 1}/{100} - X: {x}, Y: {y_start}")
+        print(f"Step {step + 1}/{100}")
 
-    # Ensure the monkey reaches the target
-    monkey.place(x=x_target, y=y_start)
-    winsound.Beep(840,500)
+    erne_monkey.place(x=x_target, y=y_start)
+    winsound.Beep(840, 500)
 
-    # Display the word at the target position
     word_label.config(text=word_to_teach)
-    
-# button=tk.Button(window,text="erne send monkey ",command=move_erne_monkey)
-# button.grid(row=6,column=1)
 
-def send_monkey_with_word():
+def send_monkey_with_word(word_number):
     global word_to_teach
-    word_to_teach = "Help"
-    threading.Thread (target=move_erne_monkey).start()
+    words = emergencyMessage.split()
+    if words:
+        random_word_index = random.randint(0, len(words) - 1)
+        word_to_teach = words[random_word_index]
+        threading.Thread(target=move_erne_monkey).start()
+    else:
+        print("Invalid word number")
 
 def move_kerne_monkey():
+    global word_to_teach
     x_start = island.winfo_x() + island.winfo_width() // 2
     y_start = island.winfo_y() + island.winfo_height() - 105
     x_target = continent.winfo_x() + continent.winfo_width() // 2
@@ -70,62 +73,60 @@ def move_kerne_monkey():
     distance_x = x_target - x_start
     step_size = distance_x / 100  # Adjust the step size as needed
 
-    # Move the monkey horizontally
     for step in range(100):
         x = x_start + step * step_size
-        monkey.place(x=x, y=y_start)
+        kerne_monkey.place(x=x, y=y_start)
+        winsound.Beep(440, 100)
         window.update()  # Update the window to show the new position
         window.after(50)  # Delay for smoother animation (adjust as needed)
 
-        #Prints monkey movement
         print(f"Step {step + 1}/{100} - X: {x}, Y: {y_start}")
 
-    # Ensure the monkey reaches the target
-    monkey.place(x=x_target, y=y_start)
- 
-button=tk.Button(window,text="kerne send monkey ",command=move_kerne_monkey)
-button.grid(row=7,column=1)   
+    kerne_monkey.place(x=x_target, y=y_start)
+    winsound.Beep(840, 500)
+    word_label.config(text=word_to_teach)
 
-#function that defines monkey that have been taught a word
-def teach_monkey_word(emergencyMessage, wordNumber):
-    # Split the emergency message into words
+def send_kerne_monkey_with_word(word_number):
+    global word_to_teach
     words = emergencyMessage.split()
-    # Check if the wordNumber is within the range of words in the message
-    if 0<= wordNumber < len(words):
-        # Get the word at the specified wordNumber
-        word_to_teach = words[wordNumber]
-
-        # Update the text on the monkey's canvas to show the word
-        monkey.create_text(50, 250, text=word_to_teach, fill="white", font=("Arial", 12, "bold"))
+    if words:
+        random_word_index = random.randint(0, len(words) - 1)
+        word_to_teach = words[random_word_index]
+        threading.Thread(target=move_kerne_monkey).start()
     else:
         print("Invalid word number")
+        
+kerne_monkey_button = tk.Button(window, text="kerne send monkey ", command=lambda: send_kerne_monkey_with_word(4))
+kerne_monkey_button.grid(row=7, column=1)
 
-teach_monkey_word(emergencyMessage, 2)
+def teach_monkey_word(emergency_message, word_number):
+    words = emergency_message.split()
+    if 0 <= word_number < len(words):
+        return words[word_number]
+    else:
+        print("Invalid word number")
+        return ""
 
+# Example usage: send the 5th word from the emergency message
+word_to_send = teach_monkey_word(emergencyMessage, 4)
 
-send_monkey_button = tk.Button(window, text="Ernesti send monkey with word", command=send_monkey_with_word)
+send_monkey_button = tk.Button(window, text=f"Ernesti send monkey with word", command=lambda: send_monkey_with_word(4))
 send_monkey_button.grid(row=8, column=1)
 
-# add five buttons to the top line of the window
-koristetta=tk.Label(window,text="").grid(row=0,column=0)
-point_button=[]
+koristetta = tk.Label(window, text="").grid(row=0, column=0)
+point_button = []
 for i in range(5):
-    button_temp=tk.Button(window,text="Points: "+str(i+1),padx=40)
-    button_temp.grid(row=0,column=i+1)
+    button_temp = tk.Button(window, text="Points: " + str(i + 1), padx=40)
+    button_temp.grid(row=0, column=i + 1)
     point_button.append(button_temp)
 
 def i_suppose_i_have_earned_so_much_points(amount_of_points):
     for i in range(5):
         point_button[i].configure(bg='gray')
-    time.sleep(1)    
+    time.sleep(1)
     for i in range(amount_of_points):
         point_button[i].configure(bg='green')
-        winsound.Beep(440+i*100,500)
+        winsound.Beep(440 + i * 100, 500)
 
-# example ...
-i_suppose_i_have_earned_so_much_points(1)
+i_suppose_i_have_earned_so_much_points(2)
 window.mainloop()
-
-#-luo toiminto säikeistystä (threading) käyttäen, 
-#jolla Ernesti voi lähettää yksittäisen apinan mukanaan yksi sana kohti manteretta.
-#Threading toimii mutta kuva bugaa
