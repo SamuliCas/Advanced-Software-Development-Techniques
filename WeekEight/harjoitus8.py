a7f75c6cef80df9469abec12bb7a1d93c9a6cae0
Samulikoulu <t1casa00@students.oamk.fi>
2023-10-12 16:17:36 +0300

    V2.0

--- WeekEight/harjoitus8.py
+++ WeekEight/harjoitus8.py
@@ -7,6 +7,7 @@ import pygame
 pygame.init()
 pygame.mixer.init()
 pygame.mixer.music.load("laughter.wav")
+pygame.mixer.music.load("eating.wav")
 
 window=tk.Tk()
 window.title("Exercise 5")
@@ -32,6 +33,9 @@ island_locations = []
 
 # Create a list to store monkeys on the island
 monkeys = []
+
+# Define a lock to prevent concurrent access to monkeys
+monkey_lock = threading.Lock()
 
 # Function to generate a unique sound effect for each monkey
 def generate_monkey_sound():
@@ -117,6 +121,60 @@ def create_new_island():
             monkey_thread = threading.Thread(target=monkey_life, args=(new_monkey,))
             monkey_thread.start()
 
+# Function to make some monkeys swim to the ocean
+def make_monkeys_swim():
+    num_monkeys_to_swim = min(len(monkeys), 5)  # Let a maximum of 5 monkeys swim
+    monkeys_swimming = random.sample(monkeys, num_monkeys_to_swim)
+    
+    for monkey in monkeys_swimming:
+        monkey['alive'] = False
+        swim_monkey_to_ocean_threaded(monkey['oval'])
+
+# Function to check if a monkey gets eaten while swimming
+def monkey_gets_eaten():
+    return random.randint(1, 100) <= 10
+
+# Modify the swim_monkey_to_ocean function
+def swim_monkey_to_ocean(monkey_oval):
+    # Get the initial and final coordinates for the monkey's animation
+    start_x, start_y, _, _ = sea_canvas.coords(monkey_oval)
+    final_x = start_x
+    final_y = sea_canvas.winfo_height()  # Move to the bottom of the canvas
+
+    eaten_sound = pygame.mixer.Sound("eating.wav")
+
+    # Calculate the number of steps for the animation
+    num_steps = 200
+
+    # Calculate the step size for each movement
+    step_x = (final_x - start_x) / num_steps
+    step_y = (final_y - start_y) / num_steps
+
+    # Create a moving oval to represent the monkey during the animation
+    moving_monkey = sea_canvas.create_oval(start_x, start_y, start_x, start_y, fill="brown")
+
+    # Animate the monkey's movement to the ocean
+    for step in range(num_steps):
+        time.sleep(0.1)  # Delay between movements
+        sea_canvas.move(monkey_oval, step_x, step_y)
+        sea_canvas.update()  # Update the canvas to show the intermediate step
+
+        if step == num_steps // 2 and monkey_gets_eaten():
+            sea_canvas.delete(moving_monkey)  # Remove the moving oval
+            sea_canvas.delete(monkey_oval)
+            # Play "laughter.wav" when a monkey dies
+            eaten_sound.play()
+            return  # Monkey gets eaten
+
+    # Remove the moving oval and update the original monkey's position
+    sea_canvas.delete(moving_monkey)
+    sea_canvas.coords(monkey_oval, final_x, final_y)
+
+# Function to make a monkey swim to the ocean in a separate thread
+def swim_monkey_to_ocean_threaded(monkey_oval):
+    swim_thread = threading.Thread(target=swim_monkey_to_ocean, args=(monkey_oval,))
+    swim_thread.start()
+
 # Function to clear all islands and monkeys
 def clear_all_islands_and_monkeys():
     global island_locations, monkeys
@@ -133,17 +191,14 @@ sea_canvas.grid(row=1, columnspan=6)
 # Create the "NEW ISLAND" button
 new_island_button = tk.Button(window, text="NEW ISLAND", command=create_new_island)
 new_island_button.grid(row=0, column=6)
-
+ 
 # Create the "CLEAR ALL" button
 clear_all_button = tk.Button(window, text="CLEAR ALL", command=clear_all_islands_and_monkeys)
 clear_all_button.grid(row=0, column=8)
 
-i_suppose_i_have_earned_so_much_points(1)
+# Create the "MAKE MONKEYS SWIM" button
+make_monkeys_swim_button = tk.Button(window, text="MAKE MONKEYS SWIM", command=make_monkeys_swim)
+make_monkeys_swim_button.grid(row=0, column=7)
+
+i_suppose_i_have_earned_so_much_points(2)
 window.mainloop()
-
-
-
-# now monkey has 1% change to die of laugther. Next we need to make: 
-# -muokkaa vastaavasti kokonaisuuteen ilmiö, jonka myötä, mikäli apina siirtyy merelle, 
-# sillä on joka sekunti noin prosentin riski tulla syödyksi hain toimesta. 
-# Lisää vastaavasti syödyksi tulemisen ääniefekti toteutukseesi.
